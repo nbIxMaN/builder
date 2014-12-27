688f1d464cbc4c7ba5c1d1e8dc4651abbf9507f6
nbIxMaN <Shulllik@gmail.com>
2014-12-27 16:35:43 +0300

    b

---
+++ Geometry/GUI/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Points = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Log = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 461);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 20);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Points
+            // 
+            this.Points.Location = new System.Drawing.Point(12, 424);
+            this.Points.Name = "Points";
+            this.Points.Size = new System.Drawing.Size(536, 20);
+            this.Points.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(383, 461);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(165, 20);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Generate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(198, 461);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(165, 20);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Is Parallelogram?";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Log
+            // 
+            this.Log.Location = new System.Drawing.Point(13, 398);
+            this.Log.Name = "Log";
+            this.Log.ReadOnly = true;
+            this.Log.Size = new System.Drawing.Size(535, 20);
+            this.Log.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Window;
+            this.panel1.Location = new System.Drawing.Point(13, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(535, 380);
+            this.panel1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 493);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Log);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Points);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Points;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox Log;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Geometry/GUI/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Graphics gmc = Graphics.FromHwnd(panel1.Handle);
+                gmc.Clear(Color.White);
+                List<Point> points = new List<Point>();
+                string coordinates = Points.Text;
+                foreach (string point in coordinates.Split(';'))
+                {
+                    double doubleX = Convert.ToDouble(point.Split(',')[0]);
+                    double doubleY = Convert.ToDouble(point.Split(',')[1]);
+                    int x = Convert.ToInt32(Math.Round(doubleX, 0));
+                    int y = Convert.ToInt32(Math.Round(doubleY, 0));
+                    points.Add(new Point(x, panel1.Height - y));
+                }
+                Point[] pointsArray = points.ToArray();
+                gmc.FillPolygon(Brushes.Aqua, pointsArray);
+                gmc.DrawPolygon(Pens.Aqua, pointsArray);
+                points.Clear();
+            }
+            catch
+            {
+                Points.Clear();
+                Points.AppendText("Please enter points");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                List<double> points = new List<double>();
+                string coordinates = Points.Text;
+                foreach (string point in coordinates.Split(';'))
+                {
+                    double x = Convert.ToDouble(point.Split(',')[0]);
+                    double y = Convert.ToDouble(point.Split(',')[1]);
+                    points.Add(x);
+                    points.Add(panel1.Height - y);
+                }
+                double[] pointsArray = points.ToArray();
+                Geometry.Geometry geom = 
+                    new Geometry.Geometry(pointsArray[0], pointsArray[1], pointsArray[2], pointsArray[3], pointsArray[4], pointsArray[5], pointsArray[6], pointsArray[7]);
+                if (geom.IsParalel)
+                {
+                    Log.Clear();
+                    Log.AppendText("True");
+                }
+                else
+                {
+                    Log.Clear();
+                    Log.AppendText("False");
+                }
+            }
+            catch
+            {
+                Points.Clear();
+                Points.AppendText("Please enter points");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            Points.Clear();
+            for (int i = 0; i < 3; i++)
+            {
+                Points.AppendText(r.Next(panel1.Width).ToString());
+                Points.AppendText(",");
+                Points.AppendText(r.Next(panel1.Height).ToString());
+                Points.AppendText(";");
+            }
+            Points.AppendText(r.Next(panel1.Width).ToString());
+            Points.AppendText(",");
+            Points.AppendText(r.Next(panel1.Height).ToString());
+        }
+    }
+}
