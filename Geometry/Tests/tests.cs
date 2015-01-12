e8612edc51fe66da25d934de70228e3cdaa9816b
nbIxMaN <Shulllik@gmail.com>
2015-01-12 05:35:33 +0300

    Test

---
+++ Geometry/Tests/tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Test
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void test1()
+        {
+            Geometry.Geometry geom = new Geometry.Geometry(190, 65, 244, 216, 307, 196);
+            Assert.IsTrue(geom.CheckDot());
+        }
+        [Test]
+        public void test2()
+        {
+            Geometry.Geometry geom = new Geometry.Geometry(84, 75, 327, 82, 92, 187);
+            Assert.IsFalse(geom.CheckDot());
+        }
+    }
+}
