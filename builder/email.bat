b79f38d911b3f12ebacb3e901f9011049fac173f
nbIxMaN <Shulllik@gmail.com>
2015-01-12 06:20:43 +0300

    sdff

--- builder/email.bat
+++ builder/email.bat
@@ -3,25 +3,24 @@ if not "%ISSTARTED%"=="STARTED" goto :EOF
 
 if "%ERRORS%"=="OK" (
 	set SUBJECT="Errors not found"
-	set BODY="My congratulations"
-	%BLAT% -subject %SUBJECT% -body %BODY% -to %EMAIL%)
+	set BODY="My congratulations")
 
 if "%ERRORS%"=="CloneSucceeded" (
 	set SUBJECT="CloneSucceeded"
-	set BODY="Clone Succeeded, please check REPOURL in settings"
-	%BLAT% -subject %SUBJECT% -body %BODY% -to %EMAIL%)
+	set BODY="Clone Succeeded, please check REPOURL in settings")
 
 if "%ERRORS%"=="BuildSucceeded" (
 	set SUBJECT="BuildSucceeded"
 	set BODY="Build Succeeded, please read Logfile"
-	%BLAT% -subject %SUBJECT% -body %BODY% -to %EMAIL% -attacht %BUILDLOG%)
+	set FILE=-attacht BUILDLOG)
 
 if "%ERRORS%"=="FileSucceeded" (
 	set SUBJECT="FileSucceeded"
-	set BODY="File Succeeded, some file not found"
-	%BLAT% -subject %SUBJECT% -body %BODY% -to %EMAIL%)
+	set BODY="File Succeeded, some file not found")
 
 if "%ERRORS%"=="TestSucceeded" (
 	set SUBJECT="TestSucceeded"
 	set BODY="Some tests failed"
-	%BLAT% -subject %SUBJECT% -body %BODY% -to %EMAIL% -attacht %TESTSLOG%)
+	set FILE=-attacht TESTSLOG)
+
+%BLAT% -subject %SUBJECT% -body %BODY% -to %EMAIL% %FILE%
